bcf80a6b5e68ae846a3d73043441cba96a1ee09d
Vishal Tyagi <vishal.tyagi@taazaa.com>
2022-09-14 13:07:38 +0530

    Updates

---
+++ precache-manifest.00674c237307bef6b4f225f37e373547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "575e26e82b6d214a801d",
+    "url": "/react-practice-repos/textutils/static/css/main.6753756e.chunk.css"
+  },
+  {
+    "revision": "575e26e82b6d214a801d",
+    "url": "/react-practice-repos/textutils/static/js/main.575e26e8.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/react-practice-repos/textutils/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "1ab0385dadbefe23080f",
+    "url": "/react-practice-repos/textutils/static/js/2.1ab0385d.chunk.js"
+  },
+  {
+    "revision": "8c135dbcb8e5b44b93a8",
+    "url": "/react-practice-repos/textutils/static/js/runtime~main.8c135dbc.js"
+  },
+  {
+    "revision": "d243977b9353f89fafd42cd3a452b29a",
+    "url": "/react-practice-repos/textutils/index.html"
+  }
+];
